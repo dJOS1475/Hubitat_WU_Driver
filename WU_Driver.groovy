d78a434bf7ec67911c9df123a195e57e6c00fabc
Cybernetic Systems <39177227+dJOS1475@users.noreply.github.com>
2022-09-25 19:31:51 +1000

    Add files via upload
    v6.0.1

--- WU_Driver.groovy
+++ WU_Driver.groovy
@@ -18,11 +18,12 @@
  *
  *  Last Update 25/09/2022
  *
- *  v6.0.1 - Added HTML Tiles for easier / prettier Dashboard configuration (thanks to @thebearmay for his extensive coaching)
- *  v5.7.0 - Added a 3rd day of forecast data "forecastDayAfterTomorrow" including Icon
- *  v5.6.5 - Fixed Polling Bug
- *  v5.6.4 - Removed extra fields due to excess events being generated
- *  v5.6.3 - Removed Snow fields due to excess events being generated
+ *	v6.0.1 - Added a 3 Day Forecast HTML Dashboard Tile 
+ *			 (thanks to @thebearmay for his extensive assistance and @sburke781 for his help with CSS)
+ *	v5.7.0 - Added a 3rd day of forecast data "forecastDayAfterTomorrow" including Icon
+ *	v5.6.5 - Fixed Polling Bug
+ *	v5.6.4 - Removed extra fields due to excess events being generated
+ *	v5.6.3 - Removed Snow fields due to excess events being generated
  *	v5.6.2 - Added 6 new fields - Thunder, Snow & UV
  *	v5.6.1 - Minor Bug Fix 
  *	v5.6.0 - add selectable language eg en-GB or en-US 
@@ -338,6 +339,7 @@ def poll1(){
     def params1 = [uri: "https://api.weather.com/v2/pws/observations/current?stationId=${pollLocation}&format=json&units=${state.unit}&apiKey=${apiKey}"]
     asynchttpGet("pollHandler1", params1)   
 }
+
 def poll2(){
     formatUnit()
     state.latt1 = (location.getLatitude())
@@ -424,8 +426,8 @@ def updateTile() {
 	html +="<div style='line-height:50%;'><br></div>"
 	html +="<div style='line-height:0.95; font-size:0.75em; text-align: left;'><br>Day: ${device.currentValue('dayAfterTomorrow')}<br></div>"
 	html +="<div style='line-height:0.95; font-size:0.75em; text-align: justify'><br>Forecast: ${device.currentValue('forecastDayAfterTomorrow')}<br></div>"
-	log.debug "html contains ${html}"
 	sendEvent(name: "html", value: "$html")
+	log.debug "html contains ${html}"
 	}
 	
 
