48fc1ddce45a1b9672876557e30733be40b6a7c8
Cybernetic Systems <39177227+dJOS1475@users.noreply.github.com>
2022-11-08 14:48:56 +1100

    Add files via upload
    v6.9.2

--- WU_Driver.groovy
+++ WU_Driver.groovy
@@ -1,63 +1,63 @@
 		
 /**
- * Wunderground Driver
- *
- *  Maintained by Derek Osborn
- *
- *  This driver was originally written by @mattw01 and @Cobra
- *  Modified and fixed by @dJOS
- *  Additional contributions by @thebearmay @sburke781 @Busthead @swade @kampto
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *  Last Update 11/08/2022
- *
- *  v6.9.1 - Enabled the manual entry of Location (lat/long) for Forecasts etc
- *  v6.8.3 - Added Error Checks when WU doesn't return all days of rain history
- *  v6.8.2 - Removed option PWS functionality - it just broke too many things - added version reporting
- *  v6.7.1 - Bug Fixes by @swade
- *  v6.7.0 - Added Rain History Tile and Today/Tonight forecast header when forecast changes by @swade
- *	v6.6.0 - Implement Weather Warning Dashboard Tile + made 12:01am default day start for new installs + Forecast Data code restructure
- *	v6.5.1 - Implement Weather Warnings and Codes 
- *	v6.4.0 - Implement Day/night switching for most forecast items
- *	v6.3.2 - Fix a rain forecast bug
- *	v6.3.1 - Bug Fix for null on line 538 error
- *	v6.3.0 - Added 3 Day Weather Forecast Dashboard tile and additional data ingestion developed by @swade 
- *	v6.2.4 - Not all instances of log.info were checking if txtEnable == true
- *	v6.2.3 - Replaced logSet with txtEnable to conform with built-in drivers and consolidate Hubitat Preference Manager entries for better user experience
- *	v6.2.2 - Actually Fixed the Day/Night ForecastDayAfterTomorrow Icon switch over bug
- *	v6.2.1 - Fixed Day/Night ForecastDayAfterTomorrow Icon switch over bug
- *	v6.2.0 - Improved formatting, fixed debug.logging and added Station Location to the Tiles
- *	v6.1.1 - Broke 3 Day FC into individual tiles due to 1024 char limit
- *	v6.0.2 - Tile bug fixes
- *	v6.0.1 - Added a 3 Day Forecast Dashboard Tile 
- *			 (thanks to @thebearmay for his extensive HTML assistance and @sburke781 for his help with CSS)
- *	v5.7.0 - Added a 3rd day of forecast data "forecastDayAfterTomorrow" including Icon
- *	v5.6.5 - Fixed Polling Bug
- *	v5.6.4 - Removed extra fields due to excess events being generated
- *	v5.6.3 - Removed Snow fields due to excess events being generated
- *	v5.6.2 - Added 6 new fields - Thunder, Snow & UV
- *	v5.6.1 - Minor Bug Fix 
- *	v5.6.0 - add selectable language eg en-GB or en-US 
- *	v5.5.0 - WU Icons now hosted on GitHub
- *	v5.4.0 - Bug Fixes
- *	v5.3.0 - Major to changes forecastToday to auto-switch to night info including fixing icons to match
- *	v5.2.0 - Modified to add forecastToday and forecastTomorrow by Derek Osborn
- *	v5.1.0 - Modified to use latitude and longitude from the hub and add cloudCover by Derek Osborn
- *	V5.0.0 - Release by @Cobra
- *	V1.0.0 - Original @mattw01 version
- *
- */
+* Wunderground Driver
+*
+*  Maintained by Derek Osborn
+*
+*  This driver was originally written by @mattw01 and @Cobra
+*  Modified and fixed by @dJOS
+*  Additional contributions by @thebearmay @sburke781 @Busthead @swade @kampto
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*  Last Update 11/08/2022
+*
+*	v6.9.2 - Enabled the manual entry of Location (lat/long) for Forecasts etc
+*	v6.8.3 - Added Error Checks when WU doesn't return all days of rain history
+*	v6.8.2 - Removed option PWS functionality - it just broke too many things - added version reporting
+*	v6.7.1 - Bug Fixes by @swade
+*	v6.7.0 - Added Rain History Tile and Today/Tonight forecast header when forecast changes by @swade
+*	v6.6.0 - Implement Weather Warning Dashboard Tile + made 12:01am default day start for new installs + Forecast Data code restructure
+*	v6.5.1 - Implement Weather Warnings and Codes 
+*	v6.4.0 - Implement Day/night switching for most forecast items
+*	v6.3.2 - Fix a rain forecast bug
+*	v6.3.1 - Bug Fix for null on line 538 error
+*	v6.3.0 - Added 3 Day Weather Forecast Dashboard tile and additional data ingestion developed by @swade 
+*	v6.2.4 - Not all instances of log.info were checking if txtEnable == true
+*	v6.2.3 - Replaced logSet with txtEnable to conform with built-in drivers and consolidate Hubitat Preference Manager entries for better user experience
+*	v6.2.2 - Actually Fixed the Day/Night ForecastDayAfterTomorrow Icon switch over bug
+*	v6.2.1 - Fixed Day/Night ForecastDayAfterTomorrow Icon switch over bug
+*	v6.2.0 - Improved formatting, fixed debug.logging and added Station Location to the Tiles
+*	v6.1.1 - Broke 3 Day FC into individual tiles due to 1024 char limit
+*	v6.0.2 - Tile bug fixes
+*	v6.0.1 - Added a 3 Day Forecast Dashboard Tile 
+*			 (thanks to @thebearmay for his extensive HTML assistance and @sburke781 for his help with CSS)
+*	v5.7.0 - Added a 3rd day of forecast data "forecastDayAfterTomorrow" including Icon
+*	v5.6.5 - Fixed Polling Bug
+*	v5.6.4 - Removed extra fields due to excess events being generated
+*	v5.6.3 - Removed Snow fields due to excess events being generated
+*	v5.6.2 - Added 6 new fields - Thunder, Snow & UV
+*	v5.6.1 - Minor Bug Fix 
+*	v5.6.0 - add selectable language eg en-GB or en-US 
+*	v5.5.0 - WU Icons now hosted on GitHub
+*	v5.4.0 - Bug Fixes
+*	v5.3.0 - Major to changes forecastToday to auto-switch to night info including fixing icons to match
+*	v5.2.0 - Modified to add forecastToday and forecastTomorrow by Derek Osborn
+*	v5.1.0 - Modified to use latitude and longitude from the hub and add cloudCover by Derek Osborn
+*	V5.0.0 - Release by @Cobra
+*	V1.0.0 - Original @mattw01 version
+*
+*/
 
 def version() {
-    return "6.9.1"
+    return "6.9.2"
 }
 
 metadata {
@@ -198,13 +198,13 @@ metadata {
             input "pollIntervalLimit", "number", title: "Poll Interval Limit:", required: true, defaultValue: 1
             input "autoPoll", "bool", required: false, title: "Enable Auto Poll"
             input "pollInterval", "enum", title: "Auto Poll Interval:", required: false, defaultValue: "5 Minutes", options: ["5 Minutes", "10 Minutes", "15 Minutes", "30 Minutes", "1 Hour", "3 Hours"]
-            input "txtEnable", "bool", title: "Enable Detailed logging", required: false, defaultValue: false
-            input "cutOff", "time", title: "New Day Starts", required: true, defaultValue: "00:01"
+                        input "cutOff", "time", title: "New Day Starts", required: true, defaultValue: "00:01"
             input "gpsCoords", "bool", title: "Use custom GPS Coordinates for Forecast?", defaultvalue: false, submitOnChange: true
 			if (gpsCoords) {
 			input "latitudeCust", "text", title: "Enter Latitude in decimals, EX: 37.48644", defaultValue: 0, width: 6, required: false
 			input "longitudeCust", "text", title: "Enter Longitude in decimals, EX: -121.932309", defaultValue: 0, width: 6, required: false}
-			        }
+			input "txtEnable", "bool", title: "Enable Detailed logging", required: false, defaultValue: false
+			}
     }
 }
 
